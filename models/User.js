50b84b2851dbdbd809d983efa8a78db0d51e0391
Nerdeee <george.larres@gmail.com>
2023-09-28 19:33:48 -0700

    added user schema

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose();
+
+const User = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    dob: {
+        type: Date,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    sex: {
+        type: String,
+        required: true
+    },
+    topGenre: {
+        type: String,
+        required: true
+    },
+    topArtist: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = User;
