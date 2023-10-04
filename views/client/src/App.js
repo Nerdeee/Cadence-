90db49e6153f838e6fb47e10931b9c4e653cb421
Joshua Vargas <joshuavargas@swuser-129-8-201-179.swlan.csufresno.edu>
2023-10-03 19:47:18 -0700

    static home page with two pages

--- views/client/src/App.js
+++ views/client/src/App.js
@@ -1,27 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
+import Login from './Login';
 
 function App() {
-    return <Navbar />;
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            Cadence
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let component 
+    switch (window.location.pathname) {
+        case "/":
+            component = <Home />
+            break
+        case "/login":
+            component = <Login />
+            break
+    }
+    return (
+    <>
+        <Navbar />
+        <div class="container">{component}</div>
+    </>
+    ) 
 }
 
 export default App;
