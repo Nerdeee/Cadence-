c81d00e198b83b8d2626a16d001556d1d66b4ad0
Nerdeee <george.larres@gmail.com>
2023-10-17 18:41:11 -0700

    User login logic

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -1,5 +1,8 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const cookie = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const path = require('path');
 
 const loginUser = async (req, res) => {
     const { user, pwd } = req.body;
@@ -7,8 +10,8 @@ const loginUser = async (req, res) => {
     if (person) {
         const verify = await bcrypt.compare(pwd, person.password);
         if (verify) {
-            //do something
-
+            //Needs JWT logic
+            res.json({ "Success": "User logged in" });
         } else res.status(401).json({ "message": "Password is incorrect" });
     } else {
         res.status(401).json({ "message": "Username does not exist" });
--- routes/login.js
+++ routes/login.js
@@ -2,9 +2,12 @@ const express = require('express');
 const { builtinModules } = require('module');
 const router = express.Router();
 const path = require('path');
+const loginUser = require('../controllers/loginUser.js');
 
 router.get('^/$|/login(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'login.html'));
 })
+
+router.post('^/$|/login(.html)?', loginUser);
 
 module.exports = router;
