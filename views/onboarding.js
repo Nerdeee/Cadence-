f7712c6fa8631ea6fa181912ba3548f1816c61ed
Nerdeee <george.larres@gmail.com>
2023-12-05 13:48:28 -0800

    Added some of the fetch code to send genre to backend and made it so the user's top genre is shown to them before submitting

--- views/onboarding.js
+++ views/onboarding.js
@@ -1,3 +1,36 @@
 const form = document.getElementById("onboardingForm");
+const submit_button = document.getElementById('submit');
+
+const baseURL = 'http://localhost:5501/onboarding';
+submit_button.addEventListener('click', postInfo);
+
+async function postInfo(e) {
+    e.preventDefault();
+    try {
+        const res = await fetch(baseURL, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: username.value,
+                password: password.value,
+                email: email.value
+            })
+        });
+
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+
+        const data = await res.json();
+        console.log(data);
+        window.location.href = "onboarding.html";
+        // Do something with the response data
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+}
+
 
 
