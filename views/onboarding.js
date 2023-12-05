3d7f7876f71a52f639618c20b02b859b7f92ab37
Nerdeee <george.larres@gmail.com>
2023-12-05 15:22:17 -0800

    Onboarding code for sending from frontend to backend partially done

--- views/onboarding.js
+++ views/onboarding.js
@@ -1,36 +1,45 @@
-const form = document.getElementById("onboardingForm");
-const submit_button = document.getElementById('submit');
+document.addEventListener('DOMContentLoaded', () => {
+    const onboardingForm = document.getElementById("onboardingForm");
+    const username = document.getElementById('username');
+    const password = document.getElementById('password');
+    const spotifyGenre = document.getElementById('mostFrequentGenreDisplay')
+    const submit_button = document.getElementById('submit');
+    const selectedRadio = document.querySelector('input[name="Music Genre"]:checked');
 
-const baseURL = 'http://localhost:5501/onboarding';
-submit_button.addEventListener('click', postInfo);
+    const baseURL = 'http://localhost:5501/onboarding';
+    submit_button.addEventListener('click', postInfo);
 
-async function postInfo(e) {
-    e.preventDefault();
-    try {
-        const res = await fetch(baseURL, {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                username: username.value,
-                password: password.value,
-                email: email.value
-            })
-        });
+    async function postInfo(e) {
+        e.preventDefault();
+        try {
+            const topGenre = "";
+            if (!spotifyGenre.textContent === "") {
+                topGenre = selectedRadio.value;
+            } else {
+                topGenre = spotifyGenre.textContent.substring(39);
+            }
+            const res = await fetch(baseURL, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username: username.value,
+                    password: password.value,
+                    topGenre: topGenre
+                })
+            });
 
-        if (!res.ok) {
-            throw new Error(`HTTP error! status: ${res.status}`);
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+
+            const data = await res.json();
+            console.log(data);
+            window.location.href = "onboarding.html";
+            // Do something with the response data
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
         }
-
-        const data = await res.json();
-        console.log(data);
-        window.location.href = "onboarding.html";
-        // Do something with the response data
-    } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
     }
-}
-
-
-
+});
