ec87632eac27fd71e4df3a702cc1e196e1b7e698
Joshua Vargas <joshuavargas@swuser-129-8-203-17.swlan.csufresno.edu>
2024-02-15 18:52:02 -0800

    commenting code, trying to learn backend code

--- controllers/registerUser.js
+++ controllers/registerUser.js
@@ -4,14 +4,14 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 const registerUser = async (req, res) => {
-    const { username, password, email } = req.body;
-    if (!username || !password || !email) { return res.status(400).json({ "message": "Username, password, and email are required when signing up" }); }
+    const { username, password, email } = req.body;         //variables from request body
+    if (!username || !password || !email) { return res.status(400).json({ "message": "Username, password, and email are required when signing up" }); }     //if any required field is blank
     else {
         const checkForDuplicate = await User.findOne({ username: username }).exec();
         if (checkForDuplicate) { return res.status(409).json({ "message": "Error: An account has aready been created using this username" }); }
         const hash = await bcrypt.hash(password, 10);
 
-        try {
+        try {   //try to create new user
             const new_user = await User.create({
                 "username": username,
                 "password": hash,
@@ -29,7 +29,8 @@ const registerUser = async (req, res) => {
             console.log(new_user);
             console.log('\nToken:', token)
             res.status(201).json({ "message": `New user created!`, "User": new_user, "token": token }); //${new_user} should only be used for testing
-        } catch (err) {                                                                                //will be replaced when project is deployed
+        } 
+        catch (err) {                                                                                //will be replaced when project is deployed
             res.status(500).json({ "message": err.message });                                          //as it is a security risk
         }
     }
