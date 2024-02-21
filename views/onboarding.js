56541d569c41bb567fd2a830a4892a6aba8dbbea
Nerdeee <george.larres@gmail.com>
2024-02-20 18:18:18 -0800

    Minor bug fixes and added file for main page scripts

--- views/onboarding.js
+++ views/onboarding.js
@@ -1,57 +1,9 @@
-/*document.addEventListener('DOMContentLoaded', () => {
-    const onboardingForm = document.getElementById("onboardingForm");
-    const username = document.getElementById('username');
-    const password = document.getElementById('password');
-    const spotifyGenre = document.getElementById('mostFrequentGenreDisplay')
-    const submit_button = document.getElementById('submit');
-    const selectedRadio = document.querySelector('input[name="Music Genre"]:checked');
-
-    const baseURL = 'http://localhost:5501/onboarding';
-    submit_button.addEventListener('click', postInfo);
-
-    async function postInfo(e) {
-        e.preventDefault();
-        try {
-            const topGenre = "";
-            if (!spotifyGenre.textContent === "") {
-                topGenre = selectedRadio.value;
-            } else {
-                topGenre = spotifyGenre.textContent.substring(39);
-            }
-            const res = await fetch(baseURL, {
-                method: 'POST',
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    username: username.value,
-                    password: password.value,
-                    topGenre: topGenre
-                })
-            });
-
-            if (!res.ok) {
-                throw new Error(`HTTP error! status: ${res.status}`);
-            }
-
-            const data = await res.json();
-            console.log(data);
-            window.location.href = "onboarding.html";
-            // Do something with the response data
-        } catch (error) {
-            console.error('There was a problem with the fetch operation:', error);
-        }
-    }
-});
-*/
-
 // onboarding.js
 const onboardingSubmit = () => {
     console.log('onborading submit button clicked')
     //document.addEventListener('DOMContentLoaded', function () {
     const onboardingFormAnswer = document.querySelector('input[name="Music Genre"]:checked');
-    const username = document.cookie;
-    const password = document.getElementById('password');
+    const username = document.cookie.token;
     const spotifyGenre = document.getElementById('mostFrequentGenreDisplay');
 
     // Log each element to check if it's found - testing only
@@ -59,10 +11,9 @@ const onboardingSubmit = () => {
         console.log('onboardingForm:', onboardingFormAnswer.value);
     }
     console.log('username:', username);
-    console.log('password:', password);
     console.log('spotifyGenre:', spotifyGenre.textContent.substring(39));
 
-    if (!onboardingFormAnswer || !username || !password || !spotifyGenre) {
+    if (!onboardingFormAnswer && !username || !spotifyGenre && !username) {
         console.log('items missing')
         //return;
     }
@@ -82,7 +33,7 @@ const onboardingSubmit = () => {
             if (spotifyGenre.textContent === "") {
                 topGenre = onboardingFormAnswer.value;
             } else {
-                topGenre = spotifyGenre.textContent.substring(39);
+                topGenre = spotifyGenre.textContent.substring(40);
             }
             console.log('topGenre is equal to: ', topGenre)          // used for testing purposes
             const res = await fetch(baseURL, {
