b0374f2ccfb43adf15afefebcdf0c0526c889b0d
Nerdeee <george.larres@gmail.com>
2024-02-27 13:47:35 -0800

    did more work on profile page

--- views/profile.js
+++ views/profile.js
@@ -1,3 +1,20 @@
-const getProfileData = () => {
+const getProfileData = async (req, res) => {
+    const baseURL = "http://localhost:5501/profile";
+    try {
+        const userData = await fetch(baseURL, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
 
+        if (userData.ok) {
+            let data = await userData.json();
+            console.log(data);
+        } else {
+            console.log("user data unable to be fetched");
+        }
+    } catch (err) {
+        console.log(err);
+    }
 }
