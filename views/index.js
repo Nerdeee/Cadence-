13ecd4994aab2ebff6eefb9798b4f86feb9961a0
Nerdeee <george.larres@gmail.com>
2024-03-03 18:33:38 -0800

    Did more work on matchmaking. Trying to resolve bug where new users aren't being displayed after liking/disliking the previous user

--- views/index.js
+++ views/index.js
@@ -1,5 +1,3 @@
-userArray = []
-
 const getSimilarUsers = async (req, res) => {
     console.log('getSimilarUsers has run from frontend');
     const baseURL = "http://localhost:5501/index";
@@ -14,24 +12,26 @@ const getSimilarUsers = async (req, res) => {
         if (getUsers.ok) {
             console.log('retrieving users from backend...');
             let responseData = await getUsers.json();
-            responseData.forEach(element => {
+            handleDisplayUsers(responseData[0]);
+            /*responseData.forEach(element => {
                 console.log(element.username, element.sex, element.topGenre, element.age, element.location, element.sexualPreference);
-                handleDisplayUsers(element.username, element.sex, element.topGenre, element.age, element.location, element.sexualPreference);
-            });
+                handleDisplayUsers(element);
+            });*/
+            console.log('getSimilarUsers function finished');
+            return responseData;
         }
-        console.log('getSimilarUsers function finished');
     } catch (err) {
         console.log(err);
     }
 }
 
-const handleDisplayUsers = (username, sex, topGenre, age, location, sexualPreference) => {
-    const showUsername = document.getElementById('showUsername').innerText = username;
-    const showSex = document.getElementById('showSex').innerText = sex;
-    const showGenre = document.getElementById('showGenre').innerText = topGenre;
-    const showAge = document.getElementById('showAge').innerText = age;
-    const showLocation = document.getElementById('showLocation').innerText = location;
-    const showSexualPreference = document.getElementById('showSexualPreference').innerText = sexualPreference;
+const handleDisplayUsers = (userObj) => {
+    const showUsername = document.getElementById('showUsername').innerText = userObj.username;
+    const showSex = document.getElementById('showSex').innerText = userObj.sex;
+    const showGenre = document.getElementById('showGenre').innerText = userObj.topGenre;
+    const showAge = document.getElementById('showAge').innerText = userObj.age;
+    const showLocation = document.getElementById('showLocation').innerText = userObj.location;
+    const showSexualPreference = document.getElementById('showSexualPreference').innerText = userObj.sexualPreference;
     // will need one for displaying the respective user's profile pic
 }
 
@@ -65,3 +65,45 @@ const likeOrDislike = async (like_or_dislike) => {
     }
     // needs logic for adding user to liked / disliked arrays in user model in database
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const indexDriverFunction = async () => {
+        let userArr = [];
+        const showUsers = document.getElementById('search-button');
+        const like_button = document.getElementById('like-button');
+        const dislike_button = document.getElementById('next-button');
+        let currIndex = 0;
+
+        showUsers.addEventListener('click', async () => {
+            userArr = await getSimilarUsers();
+            console.log(userArr);
+            if (userArr.length > 0) {
+                handleDisplayUsers(userArr[currIndex]);
+            } else {
+                alert('Sorry, no users found');
+            }
+        })
+
+        like_button.addEventListener('click', async () => {
+            await likeOrDislike("like");
+            currIndex++;
+            if (currIndex < userArr.length) {
+                handleDisplayUsers(userArr[currIndex]);
+            } else {
+                alert('Sorry, you have gone through all of the users. There are no more users to display')
+            }
+        })
+
+        dislike_button.addEventListener('click', async () => {
+            await likeOrDislike("dislike");
+            currIndex++;
+            if (currIndex < userArr.length) {
+                handleDisplayUsers(userArr[currIndex]);
+            } else {
+                alert('Sorry, you have gone through all of the users. There are no more users to display')
+            }
+        })
+    }
+
+    indexDriverFunction();
+})
