481aa6342e9e60664152e6bc5718a8eeb0610ae7
lynxzen <joshuavargasjnv@gmail.com>
2024-03-11 00:42:11 -0700

    progress on image upload

--- routes/imageupload.js
+++ routes/imageupload.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const imageUpload = require('../controllers/imageUpload');
+const authUser = require('../middlewares/authUser');
 
-router.post('/upload:userId', imageUpload.uploadPhoto);
+router.post('/upload/:userId', authUser, imageUpload.uploadPhoto);
 
 module.exports = router;
