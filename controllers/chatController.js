b3c74264bf59f0ecdb75b074c4fbd2c0a9404236
Nerdeee <george.larres@gmail.com>
2024-03-26 22:58:22 -0700

    Fixed some chat issues. Users chats now being sent to DB. Need to do some tests

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,21 +1,49 @@
-const ChatModel = require('../models/ChatModel');
+const Chat = require('../models/ChatModel');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 const archiveChat = async (req, res) => {
+    //const { otherUser, chatMessage, sentBy } = req.body;
     const { otherUser, chatMessage } = req.body;
-    const verified_token = jwt.verify(req.cookies.token, process.env.SECRET_STR)
+    const verified_token = jwt.verify(req.cookies.token, process.env.SECRET_STR);
     if (!verified_token) {
         return res.redirect('/login');
     }
 
-    console.log('from backend - message = ', chatMessage);       // for testing purposes
     const { username } = verified_token;
-    let chatName = otherUser + username;
-    const sendMsgToDB = await ChatModel.findOneAndUpdate(
+    console.log('username is = ', username);
+
+    const currentUser = await User.findOne(
         { username }
     )
-    res.status(200).json('Succesfully added message to DB');
+
+    const otherUserObject = await User.findOne(
+        { username: otherUser }
+    );
+
+    const messageObj = {
+        //from: sentBy,
+        message: chatMessage
+    }
+
+    console.log('from backend - message = ', chatMessage);       // for testing purposes
+    console.log('current user email is = ', currentUser.email);
+    console.log('otherUser email is = ', otherUserObject.email);
+    let chatName = currentUser.email + otherUserObject.email;
+    console.log('chatName = ', chatName);
+    let sendMsgToDB = await Chat.findOneAndUpdate(
+        { chatname: chatName },
+        { $push: { chats: messageObj } }
+    )
+
+    if (sendMsgToDB == null) {
+        sendMsgToDB = await Chat.create(
+            { chatname: chatName },
+            { $push: { chats: messageObj } }
+        )
+    }
+    console.log(sendMsgToDB);
+    res.status(200).json({ 'Message': 'Succesfully added message to DB' });
 }
 
 module.exports = { archiveChat };
--- views/messages.js
+++ views/messages.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('form');
   const input = document.getElementById('input');
   const messages = document.getElementById('messages');
+  const otherUsername = document.getElementById('otherUsername').innerText;
   socket.on('connection', () => {
     console.log('Connected to the server');
   })
@@ -17,19 +18,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // need to add logic for 'rooms' (see https://socket.io/docs/v4/tutorial/api-overview for more details)
 
-  socket.on('chat message', msg => {
-    /*const sendMsgToDB = await fetch('http://localhost:5501/message', {
+  socket.on('chat message', async (msg) => {
+    const sendMsgToDB = await fetch('http://localhost:5501/message', {
       method: 'PUT',
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
+        otherUser: otherUsername,
         chatMessage: msg
+        //sentBy:       <-- need to figure out how I'm gonna implement this
       })
     }).catch(error => {
       console.log('Error sending message to DB: ', error);
     })
-    console.log('msg sent to db');*/
+    console.log('msg sent to db');
     const item = document.createElement('li');
     item.textContent = msg;
     messages.appendChild(item);
